a7fb5254ce4d84c542254ee95920145c63ad564e
zenoft <106492116+zenoft@users.noreply.github.com>
2022-06-01 13:05:03 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Sample />
+  </div>
+</template>
+
+<script>
+import Sample from "@/components/Sample/Sample";
+export default {
+  name: "App",
+  components: {
+    Sample,
+  },
+};
+</script>
+
+<style />
